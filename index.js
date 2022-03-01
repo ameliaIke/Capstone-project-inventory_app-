0563e15fd0f2974639a9ae25595a38c0b97d0886
Amelia <ebube.ike24@gmail.com>
2022-03-01 20:14:40 +0100

    register.ejs page edited

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 main().catch(err =>{
